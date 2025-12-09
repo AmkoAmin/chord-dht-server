0d1132349ca8e0429d1ce5a541d2a9fcbfedece5
Amin <amin.skenderi.de@gmail.com>
2025-12-09 11:08:06 +0100

    added script

---
+++ tut/dht.py
@@ -0,0 +1,11 @@
+import math
+
+m = int(input("Was ist die Bitlänge? "))
+n = int(input("Was ist die Anzahl der Nodes? "))
+k = int(input("Welche Node? "))
+
+mod = 2 ** m   # int
+
+for i in range(m):
+    print(i)
+    print((k + 2 ** i) % mod)
